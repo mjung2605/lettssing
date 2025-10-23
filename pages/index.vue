f906261d3d329b29437c2981240d127a2a33d93d
Meike Jungilligens <mjung2605@users.noreply.github.com>
2025-10-23 17:41:06 +0200

    Bilder im Banner erneuert

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,11 @@
 <template>
         <v-carousel hide-delimiters show-arrows="hover" height="430" cycle>
-            <v-carousel-item src="/images/ls-1.jpeg" cover />
-            <v-carousel-item src="/images/ls-4.jpeg" cover />
-            <v-carousel-item src="/images/ls-9.jpg" cover />
-            <v-carousel-item src="/images/ls-10.jpg" cover />
-            <v-carousel-item src="/images/ls-11.jpg" cover />
-            <v-carousel-item src="/images/ls-12.jpg" cover />
-            <v-carousel-item src="/images/ls-13.jpg" cover />
-
+            <v-carousel-item src="/images/banner1.jpg" cover />
+            <v-carousel-item src="/images/banner2-1.jpg" cover />
+            <v-carousel-item src="/images/banner3-1.jpg" cover />
+            <v-carousel-item src="/images/banner4-1.jpg" cover />
+            <v-carousel-item src="/images/banner5-1.jpg" cover />
+            <v-carousel-item src="/images/banner6-1.jpg" cover />
         </v-carousel>
         
         <ArticleOverview />
