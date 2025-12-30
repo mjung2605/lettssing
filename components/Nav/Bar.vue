5b20acd1f103a9e504a27b1882e8826755f98e41
Meike <mjung2605@users.noreply.github.com>
2025-12-30 21:18:32 +0100

    FIX: SocMed Links verschoben

--- components/Nav/Bar.vue
+++ components/Nav/Bar.vue
@@ -14,10 +14,30 @@
         <!-- desktop nav -->
         <template v-slot:append>
             <div class="d-none d-md-flex items-center space-x-4">
+
+                <!-- Navigation -->
                 <NavButton to="/" label="Home" />
                 <NavButton to="/ueber_uns" label="Über uns" />
                 <NavButton to="/foerderung" label="Förderung" />
                 <NavButton to="/kontakt" label="Kontakt" />
+                
+                <!-- Social Media -->
+                <v-btn
+                    href="https://www.instagram.com/letts_sing"
+                    target="_blank"
+                    variant="plain"
+                    color="primary" class="font-weight-bold" style="font-size: large;">
+                    <svg-icon :path="mdiInstagram" width="24" height="24" class="mr-2" viewBox="0 0 24 24" />
+                    Instagram
+                </v-btn>
+                <v-btn
+                    href="https://www.youtube.com/channel/UCOABxYdq-XZAmkVqPe_DXYQ"
+                    target="_blank"
+                    variant="plain"
+                    color="primary" class="font-weight-bold" style="font-size: large;">
+                    <svg-icon :path="mdiYoutube" width="24" height="24" class="mr-2" viewBox="0 0 24 24" />
+                    Youtube
+                </v-btn>
             </div>
 
             <!-- mobile nav -->
@@ -25,7 +45,7 @@
                 variant="plain"
                 color="primary"
                 class="d-md-none"
-                @click="drawer = true"
+                @click="drawer = !drawer"
             ><svg-icon :path="mdiMenu" width="24" height="24" viewBox="0 0 24 24" /></v-btn>
         </template>
 
@@ -39,7 +59,9 @@
             location="right"
             class="pt-4 d-md-none h-full w-full pt-16" 
         >
+            
             <v-list density="comfortable" nav>
+            <!-- Navigation -->
                 <v-list-item
                     v-for="(item, i) in navItems"
                     :key="i"
@@ -49,6 +71,17 @@
                     :aria-label="`Link zu ${item.label}`"
                 >
                     <v-list-item-title class="pa-8" style="font-size: large; font-weight: bold;">{{ item.label }}</v-list-item-title>
+                </v-list-item>
+            <!-- Social Media (externer Link) -->
+                <v-list-item 
+                    v-for="(item, i) in socialItems"
+                    :key="i"
+                    link
+                    @click="drawer = false"
+                    :aria-label="`Link zu ${item.label}`"
+                >
+                    <v-list-item-title class="pa-8" style="font-size: large; font-weight: bold;">{{ item.label }}</v-list-item-title>
+                    <svg-icon :path="item.path" width="24" height="24" class="mr-2" viewBox="0 0 24 24" />
                 </v-list-item>
             </v-list>
         </v-navigation-drawer>    
@@ -60,15 +93,20 @@
     // svg icon renderer
     import SvgIcon from '@jamescoyle/vue-icon';
     // benötigte icons
-    import { mdiMenu } from '@mdi/js'
+    import { mdiMenu, mdiInstagram, mdiYoutube } from '@mdi/js'
 
     const drawer = ref(false)
 
     const navItems = [
-      { label: 'Home', to: '/' },
-      { label: 'Über uns', to: '/ueber_uns' },
-      { label: 'Förderung', to: '/foerderung' },
-      { label: 'Kontakt', to: '/kontakt' },
+        { label: 'Home', to: '/' },
+        { label: 'Über uns', to: '/ueber_uns' },
+        { label: 'Förderung', to: '/foerderung' },
+        { label: 'Kontakt', to: '/kontakt' },
+    ]
+
+    const socialItems = [
+        { href: 'https://www.instagram.com/letts_sing', path: 'mdiInstagram', label: 'Instagram' },
+        { href: 'https://www.youtube.com/channel/UCOABxYdq-XZAmkVqPe_DXYQ', path: 'mdiYoutube', label: 'YouTube' },
     ]
 </script>
 
