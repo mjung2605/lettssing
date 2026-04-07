3c1e5635b7f74b7dfa0137fb7444bb16e5dcea97
Meike Jungilligens <mjung2605@users.noreply.github.com>
2026-04-07 16:18:51 +0200

    CLEANUP: Heading Code gekapselt

---
+++ components/Heading.vue
@@ -0,0 +1,11 @@
+<template>
+  <component :is="'h' + level" class="pt-8"> <!-- TODO noch richtig stylen -->
+    <slot />
+  </component>
+</template>
+
+<script setup>
+    defineProps({
+      level: { type: [1,2,3,4], default: 2 }
+    })
+</script>
